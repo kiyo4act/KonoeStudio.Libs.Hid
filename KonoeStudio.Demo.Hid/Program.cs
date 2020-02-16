928e594ec8c74f8bcb583d1ced6390c21dbd2324
Juhachi Konoe <18konoe@gmail.com>
2020-02-16 17:01:34 +0900

    Update Readme with usage examples #2 (+1 squashed commits)
    Squashed commits:
    
    [7a96dfd] Prepare to create nuget package #3

--- KonoeStudio.Demo.Hid/Program.cs
+++ KonoeStudio.Demo.Hid/Program.cs
@@ -24,40 +24,40 @@ namespace KonoeStudio.Demo.Hid
                 Console.WriteLine("--------------------------------------------------------------------------------------------------------------------------");
             }
 
-            //var device = list.FirstOrDefault(deviceInfo => deviceInfo.DevicePath == @"");
-            //var writeDevice = new HidDevice(@"\\?\hid#vid_17ef&pid_a018&mi_03&col02#7&26d71292&0&0001#{4d1e55b2-f16f-11cf-88cb-001111000030}"); // LED Change Write VID  : 17ef PID: a018 Usage: 0001 UsagePage: 000b
-            //writeDevice.WriteOpen();
-            //await writeDevice.WriteRawDataAsync(new byte[] { 2, 1 }, CancellationToken.None).ConfigureAwait(false);
+            var writeDevice = new HidDevice(@"\\?\hid#vid_17ef&pid_a018&mi_03&col02#7&26d71292&0&0001#{4d1e55b2-f16f-11cf-88cb-001111000030}"); // LED Change Write VID  : 17ef PID: a018 Usage: 0001 UsagePage: 000b
+            writeDevice.WriteOpen();
+            await writeDevice.WriteRawDataAsync(new byte[] { 2, 1 }, CancellationToken.None).ConfigureAwait(false);
 
-            //var readDevice = new HidDevice(@"\\?\hid#vid_17ef&pid_60cf&col01#6&393de3ee&1&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}"); // Button Read VID  : 17ef       PID: 60cf Usage: 0001 UsagePage: ff01
-            //readDevice.ReadOpen();
-            //var tokenSource = new CancellationTokenSource();
-            //var readData = await readDevice.ReadReportAsync(CancellationToken.None).ConfigureAwait(false);
+            var readDevice = new HidDevice(@"\\?\hid#vid_17ef&pid_60cf&col01#6&393de3ee&1&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}"); // Button Read VID  : 17ef       PID: 60cf Usage: 0001 UsagePage: ff01
+            readDevice.ReadOpen();
+            var tokenSource = new CancellationTokenSource();
+            var readData = await readDevice.ReadReportAsync(CancellationToken.None).ConfigureAwait(false);
 
-            //var readTask = readDevice.ReadReportAsync(CancellationToken.None).ContinueWith(t =>
-            //{
-            //    if (t.IsFaulted)
-            //    {
-            //        Console.WriteLine("Faulted");
-            //    }
-            //    else if (t.IsCanceled)
-            //    {
-            //        Console.WriteLine("Canceled");
-            //    }
-            //    else if (t.IsCompleted)
-            //    {
-            //        Console.WriteLine($"Button: ReportId: {t.Result.ReportId}, data[0]: {t.Result.Data[0]}");
-            //        tokenSource?.Cancel();
-            //    }
-            //}).ConfigureAwait(false);
+            var readTask = readDevice.ReadReportAsync(CancellationToken.None).ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                {
+                    Console.WriteLine("Faulted");
+                }
+                else if (t.IsCanceled)
+                {
+                    Console.WriteLine("Canceled");
+                }
+                else if (t.IsCompleted)
+                {
+                    var result = t.GetAwaiter().GetResult();
+                    Console.WriteLine($"Button: ReportId: {result.ReportId}, data[0]: {result.Data[0]}");
+                    tokenSource?.Cancel();
+                }
+            }, tokenSource.Token).ConfigureAwait(false);
 
 
-            //var streamdevice = new HidDevice(@"\\?\hid#vid_17ef&pid_721f&mi_02#7&2767accc&0&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}"); // HDMI Ingest VID  : 17ef       PID: 721f Usage: 0001 UsagePage: ff00
-            //streamdevice.ReadOpen();
-            //await foreach( var item in streamdevice.GetReadReportStreamAsync().WithCancellation(tokenSource.Token))
-            //{
-            //    Console.WriteLine($"{DateTime.Now}: ReportId: {item.ReportId}, data[0]: {item.Data[0]}");
-            //}
+            var streamdevice = new HidDevice(@"\\?\hid#vid_17ef&pid_721f&mi_02#7&2767accc&0&0000#{4d1e55b2-f16f-11cf-88cb-001111000030}"); // HDMI Ingest VID  : 17ef       PID: 721f Usage: 0001 UsagePage: ff00
+            streamdevice.ReadOpen();
+            await foreach (var item in streamdevice.GetReadReportStreamAsync().WithCancellation(tokenSource.Token))
+            {
+                Console.WriteLine($"{DateTime.Now}: ReportId: {item.ReportId}, data[0]: {item.Data[0]}");
+            }
 
             Console.ReadKey();
             Console.ReadKey();
